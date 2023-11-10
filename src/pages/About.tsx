01a856ba2d00a847ccb444b52c248996bc7c2bff
Niyad <niyadpunniyath007@gmail.com>
2023-11-10 17:29:38 +0530

    2nd  commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,8 @@
 export function About() {
-    return <h1>About</h1>
+    return(
+        <>
+            <h1>About</h1>
+            <h2>This store is the best store in this world. <br />Buy something and understand about this store.</h2>
+        </>
+    ) 
 }
